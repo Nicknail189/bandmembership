294e480575154bf671b17867779e008902e9dced
creed <creedmckinnoncreed@gmail.com>
2023-09-15 04:37:19 -0500

    abb

--- components/Tour.vue
+++ components/Tour.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="p-10 rounded-md bg-gray-100 text-[14px] font-bold">
-        <p>The Powderworkers! Get a chance to go on 10 dates travel tour with your favourite band "MIDNIGHT OIL" for just $10,000. Send a message with the theme "10 DATES TRAVEL TOUR" to <span class="text-[16px] font-extrabold text-orange-700">midnightoil@membershipconsultant.xyz</span></p>
+        <p>The Powderworkers! Get a chance to go on 10 dates travel tour with your favourite band "MIDNIGHT OIL" for just $10,000. Send a message with the theme "10 DATES TRAVEL TOUR" to <span class="text-[12px] md:text-[14px] font-extrabold text-orange-700">midnightoil@membershipconsultant.xyz</span></p>
     </div>
 </template>
